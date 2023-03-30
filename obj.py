91e05a3f77ad349dbaf104caabec26b10ea8208e
Lea2187 <chocolatshym@gmail.com>
2023-03-30 12:32:03 +0200

    Update

--- obj.py
+++ obj.py
@@ -3,3 +3,8 @@ class Obj:
         self.id=id
         self.x=x
         self.y=y
+
+class Stone(Obj):
+    def __init__(self, id: str, x: float, y: float,type) -> None:
+        self.type=type
+        super().__init__(id, x, y)
