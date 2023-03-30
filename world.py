003c1914ca994a60bca04f519724276735ab3e9d
asqool <axel.lleres@gmail.com>
2023-03-30 12:39:40 +0200

    update

--- world.py
+++ world.py
@@ -1,5 +1,7 @@
 import pygame as py
+from obj import *
 class World:
-    def __init__(self,name,backgroung:py.surface):
+    def __init__(self,name,backgroung:py.surface,objs:list[Obj]=list()):
         self.name=name
         self.bg=backgroung
+        self.objects=objs
