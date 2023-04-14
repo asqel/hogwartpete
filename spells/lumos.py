fd87151cf3fa425601473dea324a9a5ab64d8ac9
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-14 15:34:29 +0200

    aled

--- spells/lumos.py
+++ spells/lumos.py
@@ -5,7 +5,6 @@ class Lumos(Spell):
 
     def __init__(self, sender: uuid.UUID) -> None:
         super().__init__(LUMOS, sender)
-    
+
     def shoot(self, world, mouse_pos: Vec):
         ...
-        
--- spells/unforgivable_curses.py
+++ spells/unforgivable_curses.py
@@ -5,29 +5,26 @@ class Avada_kedavra(Spell):
     def __init__(self, sender: uuid.UUID) -> None:
         super().__init__(AVADA_KEDAVRA, sender)
     
-    def shoot(self,world,mouse_pos:Vec):
+    def shoot(self, world, mouse_pos: Vec):
         for i in world.entities:
-            if((mouse_pos-i.pos).squareLength()<=4*16**2):
-                i.pv=0
-                
-IMPERO="impero"
+            if((mouse_pos - i.pos).squareLength() <= 4 * 16**2):
+                i.pv = 0
+
+IMPERO = "impero"
 class Impero(Spell):
     def __init__(self, sender: uuid.UUID) -> None:
         super().__init__(IMPERO, sender)
     
-    def shoot(self,world,mouse_pos:Vec):
-        pass#TODO : implement Impero:shoot
-                
+    def shoot(self, world, mouse_pos: Vec):
+        pass    #TODO : implement Impero:shoot
 
-CRUCIO="crucio"
+CRUCIO = "crucio"
 class Crucio(Spell):
     def __init__(self, sender: uuid.UUID) -> None:
         super().__init__(CRUCIO, sender)
     
     def shoot(self,world,mouse_pos:Vec):
         pass#TODO : implement Crucio:shoot
-                
-
                 
 
 registerSpell(Avada_kedavra,AVADA_KEDAVRA)
